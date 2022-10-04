217652b2a051b0fc64878e00c10e9fdf383c9602
Rod Stewart <rodstewart@gmail.com>
2022-10-04 11:17:10 -0500

    Adds descriptions to outputs

--- output.tf
+++ output.tf
@@ -3,18 +3,3 @@ output "kube_config" {
   description = "Raw Kube config file to use for access to the cluster."
   sensitive   = true
 }
-
-output "oidc_issuer_url" {
-  value       = azurerm_kubernetes_cluster.this.oidc_issuer_url
-  description = "OIDC Issuer URL to be used for token issuance."
-}
-
-output "key_vault_name" {
-  value       = azurerm_key_vault.this.name
-  description = "Name of the Key Vault."
-}
-
-output "resource_group_name" {
-  value       = azurerm_resource_group.this.name
-  description = "Name of the Resource Group."
-}
