4c83fd55ffada725a5346d344f902fc85e09189a
Rod Stewart <rodstewart@gmail.com>
2022-10-06 13:18:25 -0500

    Adds terraform cloud backend

--- provider.tf
+++ provider.tf
@@ -1,5 +1,14 @@
 terraform {
   required_version = ">=1.2.7"
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "Rod-training"
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "tf_akv_aks_demo"
+    }
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
